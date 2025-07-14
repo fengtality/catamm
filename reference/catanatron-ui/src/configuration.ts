e9531c7ee36f8e95a31307427d8f6b9cd2cb2d46
Michael Feng <mifeng@gmail.com>
2025-07-14 09:58:14 -0700

    cleanup: deleted references

--- reference/catanatron-ui/src/configuration.ts
+++
@@ -1,2 +0,0 @@
-export const API_URL: string =
-  import.meta.env.CTRON_API_URL || "http://localhost:5001";
--- reference/settlers_of_catan_RL/game/components/buildings.py
+++
@@ -1,7 +0,0 @@
-from game.enums import BuildingType, PlayerId
-
-class Building(object):
-    def __init__(self, type: BuildingType, owner: PlayerId, corner: None):
-        self.type = type
-        self.owner = owner
-        self.corner = corner
